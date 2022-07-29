5d16adc58385979d2262bc708e432a7ac17bb24f
Thiago Torres <thiago.tcosta1@gmail.com>
2022-07-29 12:20:45 -0300

    Initial Changes

---
+++ lib/pages/product_overview_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductOverviewPage extends StatelessWidget {
+  const ProductOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shop'),
+      ),
+    );
+  }
+}
